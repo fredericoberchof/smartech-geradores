bff2d91e3608562048f7684f93612e981de94f70
Frederico Berchof <fredericoberchof@Fredericos-MacBook-Air.local>
2024-01-17 22:28:28 -0300

    novo main e fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { CoverComponent } from "./components/cover/cover.component";
+import { MainComponent } from './components/main/main.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, CoverComponent, MainComponent]
 })
 export class AppComponent {
   title = 'projeto-smartech';
