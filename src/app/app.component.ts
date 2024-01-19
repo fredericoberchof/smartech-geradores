1a07fbc8cb9375aaccfe88c846734defe7f2bbef
Frederico Berchof <fredericoberchof@Fredericos-MacBook-Air.local>
2024-01-19 08:15:32 -0300

    inicio footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { CoverComponent } from "./components/cover/cover.component";
 import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, CoverComponent, MainComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, CoverComponent, MainComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'projeto-smartech';
