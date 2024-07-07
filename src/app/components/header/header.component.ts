653458f5e371100d3119d93f2f3ef97dc2a39e69
Frederico <frederico@mngs.com.br>
2024-07-06 21:12:17 -0300

    fixes footer; header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private el: ElementRef) {}
 
+
+  openWhatsApp(): void {
+    const phoneNumber = '554830542237';
+    const url = `https://api.whatsapp.com/send?phone=${phoneNumber}`;
+    window.open(url, '_blank');
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
